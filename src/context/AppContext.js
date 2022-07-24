7e2cf5b2c7c1071b158c2eed64434578583aca72
Arzav Jain <66370916+arzav18@users.noreply.github.com>
2022-07-25 04:22:31 +0530

    Add files via upload

---
+++ src/context/AppContext.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createContext, useReducer } from 'react';
+
+const AppReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_EXPENSE':
+            return {
+                ...state,
+                expenses: [...state.expenses, action.payload],
+            };
+            case 'DELETE_EXPENSE':
+                return {
+                    ...state,
+                    expenses: state.expenses.filter(
+                        (expense)=> expense.id !== action.payload
+                        ),
+                };
+        default:
+            return state;
+    }
+};
+
+const initialState = {
+    budget: 2000,
+    expenses: [
+        { id: 12, name: 'Shopping', cost: 50},
+        { id: 13, name: 'Holiday', cost: 300},
+        { id: 14, name: 'Car Service', cost: 70},
+        { id: 15, name: 'Fuel', cost: 40},
+        { id: 16, name: 'Child Care', cost: 500},
+    ],
+};
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (
+    <AppContext.Provider 
+        value = {{
+           budget: state.budget,
+           expenses: state.expenses,
+           dispatch,
+        }}
+    >
+        {props.children}
+    </AppContext.Provider>
+    );
+};
